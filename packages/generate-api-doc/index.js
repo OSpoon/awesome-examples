13aa7fc7cbe748c571b507cbd222a776869ab17c
xin.zhang1 <zxin088@gmail.com>
2022-10-18 11:05:13 +0100

    fix: 移除测试文件

--- packages/generate-api-doc/index.js
+++
@@ -1,4 +0,0 @@
-const path = require("path");
-
-const loc = path.parse(`C:\Users\\xin.zhang1\\Documents\\GitHub\\awesome-examples\\packages\\generate-api-doc\\__tests__\\fixtures\\example\\actual.ts`)
-console.log(loc)
