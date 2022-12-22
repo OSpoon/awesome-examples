63f0add4b7eb735e90a11cac5d602ccc3f97a34b
小鑫同学 <zxin088@gmail.com>
2022-12-22 14:59:22 +0800

    配置拖拽面板、框选、分组插件

---
+++ packages/logic-flow-plugin-dndpanel-example/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import LogicFlow from "@logicflow/core";
+import { Group } from "@logicflow/extension";
+import "@logicflow/core/dist/style/index.css";
+import "@logicflow/extension/lib/style/index.css";
+
+import { onMounted, ref } from "vue";
+
+const container = ref();
+const lf = ref<LogicFlow>();
+const graphData = {
+  nodes: [
+    {
+      type: 'group',
+      x: 300,
+      y: 300,
+      children: ["node_2"]
+    },
+    {
+      id: 'node_2',
+      type: 'circle',
+      x: 300,
+      y: 300
+    }
+  ]
+}
+onMounted(() => {
+  lf.value = new LogicFlow({
+    container: container.value,
+    grid: true,
+    plugins: [Group],
+  })
+  lf.value.render(graphData);
+})
+</script>
+
+<template>
+  <div ref="container" class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  width: 800px;
+  height: 600px;
+}
+</style>
---
+++ packages/logic-flow-plugin-menu-example/src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import LogicFlow from "@logicflow/core";
+import { Menu } from "@logicflow/extension";
+import "@logicflow/core/dist/style/index.css";
+import "@logicflow/extension/lib/style/index.css";
+import { onMounted, ref } from "vue";
+
+const container = ref();
+const lf = ref<LogicFlow>();
+const graphData = {
+  nodes: [
+    {
+      id: 'node_1',
+      type: 'rect',
+      x: 100,
+      y: 100
+    },
+    {
+      id: 'node_2',
+      type: 'circle',
+      x: 300,
+      y: 200
+    }
+  ],
+  edges: [
+    {
+      sourceNodeId: 'node_1',
+      targetNodeId: 'node_2',
+      type: 'polyline'
+    }
+  ]
+};
+onMounted(() => {
+  lf.value = new LogicFlow({
+    container: container.value,
+    grid: true,
+    plugins: [Menu]
+  })
+  lf.value.render(graphData);
+})
+</script>
+
+<template>
+  <div ref="container" class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  width: 500px;
+  height: 400px;
+}
+</style>
